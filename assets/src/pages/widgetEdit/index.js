6193e8eb34562f1f73e36cece8339c1bca450468
bencode <bigyu100@gmail.com>
2020-03-08 14:38:18 +0800

    start to create workbench

--- assets/src/pages/widgetEdit/index.js
+++ assets/src/pages/widgetEdit/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import style from './style.scss';
+import { Workbench } from '@/workbench';
+import './style.scss';
 
 
 const App = () => {
   return (
-    <div className={style.app}>Hello</div>
+    <Workbench />
   );
 };
 
---
+++ assets/src/workbench/Workbench/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import $t from 'prop-types';
+import style from './style.scss';
+
+const Workbench = () => {
+  return (
+    <div className={style.workbench}>
+      Hello
+    </div>
+  );
+};
+
+Workbench.propTypes = {
+
+};
+
+export default Workbench;
