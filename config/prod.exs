a1e852fe3f231446f1ca0124ba5b061307e1ccc4
bencode <bigyu100@gmail.com>
2020-03-09 12:33:17 +0800

    set config server: true

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,12 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :pageviver, PageviverWeb.Endpoint,
-  url: [host: "pageviver.com", port: 80],
+  server: true,
+  http: [port: 4000],
+  url: [host: "pageviver.com", port: 443, scheme: "https"],
   cache_static_manifest: "priv/static/cache_manifest.json"
+
+# force_ssl: [rewrite_on: [:x_forwarded_proto], hsts: true]
 
 # Do not print debug messages in production
 config :logger, level: :info
