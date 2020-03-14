9243cc752dab1c4cb821b2a6df4f5fba86b2247b
bencode <bigyu100@gmail.com>
2020-03-15 01:15:08 +0800

    upgrade assets config

--- config/prod.exs
+++ config/prod.exs
@@ -13,6 +13,7 @@ config :pageviver, PageviverWeb.Endpoint,
   server: true,
   http: [port: 4000],
   url: [host: "pageviver.com", port: 443, scheme: "https"],
+  static_url: [host: "pageviver.oss-cn-shanghai.aliyuncs.com", port: 443, scheme: "https"],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # force_ssl: [rewrite_on: [:x_forwarded_proto], hsts: true]
