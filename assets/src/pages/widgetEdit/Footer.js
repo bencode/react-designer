1959ebf79a93c58ace54e4972128ff6c13d9673d
bencode <bigyu100@gmail.com>
2020-03-17 23:40:17 +0800

    refactor

--- assets/src/pages/widgetEdit/Footer.js
+++ assets/src/pages/widgetEdit/Footer.js
@@ -4,7 +4,7 @@ import style from './style.less';
 const Footer = () => {
   return (
     <div className={style.footer}>
-      <span className="smaller">&copy;2020 pageviver.com 浙ICP备16013774号-4</span>
+      <span className="copyright">&copy;2020 pageviver.com 浙ICP备16013774号-4</span>
     </div>
   );
 };
