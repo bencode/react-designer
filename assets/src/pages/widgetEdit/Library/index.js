c0e42f889c60bacd854c58ff001a64c791c5b102
bencode <bigyu100@gmail.com>
2020-03-20 23:08:01 +0800

    update

--- assets/src/pages/widgetEdit/Library/index.js
+++ assets/src/pages/widgetEdit/Library/index.js
@@ -2,15 +2,13 @@ import React from 'react';
 import style from './style.less';
 
 const Library = () => {
-  const basics = [
-  ];
   return (
     <div className={style.library}>
       <div className="group">
-        <div className="title">Basic</div>
+        <div className="title">Elements</div>
         <div className="body">
           <ul>
-            <li>div</li>
+            <li></li>
           </ul>
         </div>
       </div>
