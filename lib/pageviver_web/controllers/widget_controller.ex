1197e05bb997c352b6d35b92911c390de1e22cde
bencode <bigyu100@gmail.com>
2020-03-21 19:57:18 +0800

    renmae widgetEdit to design

--- lib/pageviver_web/controllers/widget_controller.ex
+++ lib/pageviver_web/controllers/widget_controller.ex
@@ -2,6 +2,6 @@ defmodule PageviverWeb.WidgetController do
   use PageviverWeb, :controller
 
   def new(conn, _param) do
-    render(conn, "edit.html", assets: "widgetEdit")
+    render(conn, "edit.html", assets: "design")
   end
 end
