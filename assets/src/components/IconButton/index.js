e1afada568a426030b519596c505a13226f3b0fd
bencode <bigyu100@gmail.com>
2020-03-25 00:17:07 +0800

    pass lint

--- assets/src/components/IconButton/index.js
+++ assets/src/components/IconButton/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import cx from 'classnames';
 import $t from 'prop-types';
-import './style.less'
+import './style.less';
 
 const IconButton = ({ icon, className, ...props }) => {
   return (
