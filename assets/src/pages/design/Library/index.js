4f6341c21b88c2e72e444d9fbd2815b6d543ff37
bencode <bigyu100@gmail.com>
2020-03-25 00:11:21 +0800

    pass stylelint

--- assets/src/pages/design/Library/index.js
+++ assets/src/pages/design/Library/index.js
@@ -3,11 +3,6 @@ import style from './style.less';
 
 
 const Library = () => {
-  const hidden = true;
-  if (hidden) {
-    return null;
-  }
-
   return (
     <div className={style.library}>
       <div className="header">Library</div>
