4723da11d02b57b129a6a6ab376b6c5739abea01
bencode <bigyu100@gmail.com>
2020-03-24 13:20:24 +0800

    show

--- assets/src/pages/design/Library/index.js
+++ assets/src/pages/design/Library/index.js
@@ -3,11 +3,16 @@ import style from './style.less';
 
 
 const Library = () => {
+  const hidden = true;
+  if (hidden) {
+    return null;
+  }
+
   return (
     <div className={style.library}>
       <div className="header">Library</div>
       <div className="group">
-        <div className="title">Elements</div>
+        <div className="title">ELements</div>
         <div className="body">
         </div>
       </div>
