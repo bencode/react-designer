0f0a64135115b73437f9231c581a831d618404a3
bencode <bigyu100@gmail.com>
2020-03-29 00:32:08 +0800

    add github icon

--- assets/src/pages/design/TopBar/index.js
+++ assets/src/pages/design/TopBar/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import $t from 'prop-types';
+import GithubIcon from '@/images/github.svg';
 import style from './style.less';
 
 const TopBar = () => {
   return (
     <div className={style.topBar}>
       TopBar
+      <a className="github" href="https://github.com/lesscap/pageviver">
+        <GithubIcon />
+      </a>
     </div>
   );
 };
