cedaf1fe1bc249f4ccd205a6fce8f7f6ccf66c43
bencode <bigyu100@gmail.com>
2020-03-31 17:25:16 +0800

    add form fields

--- assets/src/pages/design/Form/index.js
+++ assets/src/pages/design/Form/index.js
@@ -5,7 +5,46 @@ import style from './style.less';
 const Form = () => {
   return (
     <div className={style.form}>
-      Form
+      <section className={style.section}>
+        <header>Layout</header>
+        <div className="body">
+          <div className="field">
+            <label>display</label>
+            <div className="control">
+              <select>
+                <option>block</option>
+                <option>inline</option>
+                <option>inline-block</option>
+                <option>flex</option>
+                <option>inline-flex</option>
+              </select>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className={style.section}>
+        <header>Box Model</header>
+        <div className="body">
+          <div className="field">
+            <label>width</label>
+            <div className="control">
+              <input type="number" />
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className={style.section}>
+        <header>Typography</header>
+      </section>
+      <section className={style.section}>
+        <header>Visual</header>
+      </section>
+      <section className={style.section}>
+        <header>Animation</header>
+      </section>
+      <section className={style.section}>
+        <header>Misc</header>
+      </section>
     </div>
   );
 };
